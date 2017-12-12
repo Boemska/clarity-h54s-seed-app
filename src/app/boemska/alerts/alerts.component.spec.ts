e0eec10782f584e2dfb01a4c414ba6c2049ea667
Bojan Djurdjevic <bojan88@gmail.com>
2017-12-12 10:32:32 +0100

    Updated packages, fixed tests, replaced non unique ID on user-nav-dropdown, and replaced clarity variables with hardcoded values

--- src/app/boemska/alerts/alerts.component.spec.ts
+++ src/app/boemska/alerts/alerts.component.spec.ts
@@ -18,8 +18,4 @@ describe('AlertsComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
