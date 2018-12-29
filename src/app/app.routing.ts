41b6372ea8a10405f341f921858e767121c748c9
Nikola Markovic <nik@boemskats.com>
2018-12-29 07:56:19 -0500

    Update routing module to useHash so that it works with AppFactory refreshes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -29,4 +29,4 @@ export const ROUTES: Routes = [
   { path: '**', component: NotFoundComponent }
 ];
 
-export const ROUTING: ModuleWithProviders = RouterModule.forRoot(ROUTES);
+export const ROUTING: ModuleWithProviders = RouterModule.forRoot(ROUTES, { useHash: true });
